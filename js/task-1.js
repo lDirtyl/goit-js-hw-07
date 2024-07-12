9e0c0db33fdaeedf0b609ae74b3b9530b72ba6d0
lDirtyl <andrii.mashtaler@gmx.at>
2024-07-12 13:20:08 +0200

    change consol.log message

--- js/task-1.js
+++ js/task-1.js
@@ -2,13 +2,13 @@ const categoriesList = document.querySelector('#categories');
 
 const items = categoriesList.querySelectorAll('.item');
 
-console.log(items.length);
+console.log(`Number of categories: ${items.length}`);
 
 items.forEach(item => {
   const categoryTitle = item.querySelector('h2').textContent;
 
   const elementsCount = item.querySelectorAll('ul li').length;
 
-  console.log(categoryTitle);
-  console.log(elementsCount);
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${elementsCount}`);
 });
