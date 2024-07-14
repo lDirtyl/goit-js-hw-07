67823a9d3de80f8feb09010db3a13849c439d039
lDirtyl <andrii.mashtaler@gmx.at>
2024-07-14 12:42:46 +0200

    add button class to task-5-html and finish task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const widgetRef = document.querySelector('.widget');
+const buttonRef = widgetRef.querySelector('.change-color');
+const colorSpanRef = widgetRef.querySelector('.color');
+
+buttonRef.addEventListener('click', () => {
+  const hexColor = getRandomHexColor();
+  colorSpanRef.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
